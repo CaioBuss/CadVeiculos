42b96bed92f06c57787418e604847fae2c714900
Eivor Buss <caiofdab@gmail.com>
2026-04-05 21:05:39 -0300

    Remocer por nome

--- CadVeiculo.java
+++ CadVeiculo.java
@@ -6,11 +6,13 @@ List<String> veiculos = new ArrayList<>();
 void main(){
     IO.println("Bem vindo ao Sistema CadVeiculos");
     String menu = """
-            MENU DE OPÇÕES
-            1 - Cadastrar Veículo;
-            2 - Listar Veículos;
-            3 - Remover Veículo;
-            0 - Sair
+        1 - Cadastrar Veículo
+        2 - Listar Veículos
+        3 - Remover por índice
+        4 - Buscar veículo
+        5 - Editar veículo
+        6 - Remover por nome
+        0 - Sair
             """;
     int opcao;
     do {
@@ -43,18 +45,84 @@ void main(){
 }
 
 
-
+boolean existeVeiculo(String nome) {
+    for (int i = 0; i < veiculos.size(); i++) {
+        if(veiculos.get(i).equalsIgnoreCase(nome)) {
+            return true;
+        }
+    }
+    return false;
+}
 void cadastrar() {
     String veiculo = IO.readln("Digite o nome do novo veículo: ");
     veiculo = veiculo.trim();
-    if (veiculo.isEmpty())
+    if (veiculo.isEmpty()) {
         IO.println("Nome do veículo inválido!");
-    else
+        return;
+    }
+    if (existeVeiculo(veiculo)) {
+        IO.println("Veiculo ja existe!");
+        return;
+
+    } else {
         veiculos.add(veiculo);
+    }
 }
 
 void listar() {
+
+    if (veiculos.size() ==0) {
+        IO.println("Lista Vazia!");
+        return;
+    }
+}
+void ordenar() {
     for (int i = 1; i <= veiculos.size() ; i++) {
+        for (int j = 0; j < veiculos.size() - 1; j++) {
+            String atual = veiculos.get(j);
+            String proximo = veiculos.get(j + 1);
+            if (atual.compareToIgnoreCase(proximo) > 0 ) {
+                veiculos.set(j, proximo);
+                veiculos.set(j + 1, atual);
+            }
+
+        }
         IO.println(i + " - " + veiculos.get(i - 1));
     }
+}
+
+void buscar() {
+    if (veiculos.size() == 0) {
+    IO.println("Lista Vazia!");
+        return;
+    }
+    String nome = IO.readln("Digite o nome do veiculo a ser buscado: ");
+    boolean encontrado = false;
+    for (int i = 0; i < veiculos.size(); i++) {
+        if (veiculos.get(i).equalsIgnoreCase(nome)) {
+            IO.println("Veículo encontrado no índice: " + i);
+            encontrado = true;
+            break;
+        }
+    }
+    if (!encontrado) {
+        IO.println("Veiculo não encontrado!");
+    }
+    IO.println("Total de veiculos:" + veiculos.size());
+}
+
+void removerPorIndice() {
+    if (veiculos.size() == 0) {
+        IO.println("Lista Vazia!");
+        return;
+    }
+
+    int indicce = Input.scanInt("Digite o índice do veículo a ser removido: ");
+    if(indice < 0 || indice >= veiculos.size()) {
+        IO.println("incice inválido!");
+        return;
+    }
+    veiculos.remove(indice);
+    IO.println("Veículo removido com sucesso!");
+
 }
